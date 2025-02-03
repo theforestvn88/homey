c344e8a4f5cfda7e09d9fffa146214c609ca183c
Lam Phan <theforestvn88@gmail.com>
2025-02-03 16:24:31 +0700

    restrict comment length

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,6 @@
 class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :project
+
+  validates_length_of :content, in: 10..50
 end
--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -3,4 +3,8 @@ require 'rails_helper'
 RSpec.describe Comment, type: :model do
   it { should belong_to(:user) }
   it { should belong_to(:project) }
+  it { should validate_length_of(:content)
+        .is_at_least(10)
+        .is_at_most(50)
+      }
 end
