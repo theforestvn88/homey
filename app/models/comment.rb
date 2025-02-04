c6e1516a001b25329c5937abd07e69046c57ed6f
Lam Phan <theforestvn88@gmail.com>
2025-02-04 13:17:37 +0700

    udpate comment restrict length

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,5 +2,5 @@ class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :project
 
-  validates_length_of :content, in: 10..50
+  validates_length_of :content, in: 10..200
 end
--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -5,6 +5,6 @@ RSpec.describe Comment, type: :model do
   it { should belong_to(:project) }
   it { should validate_length_of(:content)
         .is_at_least(10)
-        .is_at_most(50)
+        .is_at_most(200)
       }
 end
