68c5d2ab394de84acaf62e6721b56f55c148e104
Lam Phan <theforestvn88@gmail.com>
2025-02-06 18:41:14 +0700

    update db seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,2 +1,4 @@
-user = User.create(email: "test@example.com", password: "123456")
-Project.create(title: "Project 1", user_id: user.id)
+user1 = User.create(email: "test1@example.com", password: "123456")
+user2 = User.create(email: "test2@example.com", password: "123456")
+project = Project.create(title: "Project 1", description: "This is description of the Project 1 ...", user_id: user1.id)
+Assignment.create(project_id: project.id, user_id: user2.id)
