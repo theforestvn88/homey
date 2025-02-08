c16fe2dabd9e9c6b043de20e59042195098d0836
Lam Phan <theforestvn88@gmail.com>
2025-02-08 10:04:19 +0700

    update ui

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,5 +1,5 @@
 class ProjectsController < ApplicationController
-  before_action :authenticate_user!, except: %i[ index show ]
+  before_action :authenticate_user!
   before_action :set_project, except: %i[ index new create ]
 
   rate_limit to: 1, within: 1.minute,
